a2f01dd571c96a81dfb9c61abfe3259e8acde261
Lucas DiCioccio <lucas@dicioccio.fr>
2015-09-15 01:03:23 +0100

    Categorized species of millipedes.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # millipede-haskell
 :bug: Print a beautiful millipede
+
+# Usage
+
+```
+# prints a millipede of species 0
+$ millipede
+
+# prints a millipede of species n, scientists have discovered 1309 species of millipedes to date
+$ millipede <n>
+```
--- app/Main.hs
+++ app/Main.hs
@@ -4,12 +4,24 @@ module Main (main) where
 
 import System.Environment (getArgs)
 
-millipede = zipWith (++) wiggle arthropod
-arthropod = "╚⊙ ⊙╝":(repeat "╚═(███)═╝")
+eyes = cycle ["⦿⦿", "⦾⦾", "⟐⟐", "⪽⪾", "⨷⨷", "⨸⨸", "☯☯", "⊙⊙", "⊚⊚", "⊛⊛", "⨕⨕", "⊗⊗", "⊘⊘", "⊖⊖", "⁌⁍", "✩✩", "❈❈"]
+shells = cycle ["()", "{}", "[]", "⨭⨮", "⨴⨵", "⊆⊇", "∣∣"]
+bodies = cycle ["███", "XXX", "⧱⧰⧱", "⧯⧮⧯", "⧲⧳⧲","🁢🁢🁢", "✚✚✚", "⧓⧒⧑", "⦁⦁⦁", "☗☗☗", "❝❝❝"]
+
+
+arthropod k = (face k):(repeat $ body k)
+
+face k = let (l:r:[]) = eyes !! k
+         in "╚" ++ l:" " ++ r:"╝"
+body k = let (l:r:[]) = shells !! k
+             c = bodies !! k
+         in "╚═" ++ l:c ++ r:"═╝"
+
 wiggle = map (flip replicate ' ') (4 : cycle [2,1,0,1,2,3,4,3])
+millipede = zipWith (++) wiggle . arthropod
 
 main :: IO ()
 main = getArgs >>= \case
-        []    -> beautiful millipede
-        (x:_) -> beautiful $ take (1 + read x) millipede
-        where beautiful = putStrLn . unlines
+        []     -> beautiful $ millipede 0
+        (x:[]) -> beautiful $ millipede (read x)
+        where beautiful = putStrLn . unlines . take 20
